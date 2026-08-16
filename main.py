c2b11b5061c07543669efcd5dc265e2fc1f520e5
Habibullo22 <habibulloabidjonov555@gmail.com>
2026-08-17 00:04:56 +0500

    Change ADMIN_ID default and improve error handling
    Updated ADMIN_ID default value and error message for missing BOT_TOKEN.

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("8611335484:AAHoH8mg1OO9V7PDB3wUlZ6wU3HxsQx7avY")
-ADMIN_ID = int(os.getenv("5815294733", "0"))
+ADMIN_ID = int(os.getenv("5815294733", "1"))
 
 if not BOT_TOKEN:
-    raise RuntimeError("8611335484:AAHoH8mg1OO9V7PDB3wUlZ6wU3HxsQx7avY")
+    raise RuntimeError("BOT_TOKEN Secrets ichida topilmadi!")
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
